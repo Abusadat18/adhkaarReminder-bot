1339d1c12cde681c8f9bca98b846978afe044cf7
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-08-15 08:47:26 +0530

    Check the bot working

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Telegraf } = require('telegraf');
+
+const app = express();
+const bot = new Telegraf(process.env.TOKEN);
+
+app.use(bodyParser.json());
+
+bot.command('start', (ctx) => {
+  ctx.reply('Hello! Your bot is up and running.');
+});
+
+app.post('/telegram-webhook', (req, res) => {
+  bot.handleUpdate(req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+bot.launch();
